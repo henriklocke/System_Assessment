8a4cfe3320b2a277e772554d2fb111957eef8be6
henriklocke <henriklocke@hotmail.com>
2023-03-07 08:35:57 -0800

    Add files via upload
    Made Result_Specifications.csv only required when catchments are imported

---
+++ System_Assessment_Variables.py
@@ -0,0 +1,30 @@
+##AFTER UPDATE AND SAVE YOU MUST RESTART THE KERNEL IN JUPYTER NOTEBOOK TO UPDATE VARIABLES!
+
+##Remember to insert r in front of all paths, e.g. r"J:\SEWER_AREA_MODELS\FSA\03_SIMULATION_WORK\Calibration_2022\MODEL"
+
+# model_area = "VSA"
+
+# result_specs_csv = r"J:\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\VSA_Event_Maps_Test_Jupyter\Import_Specs\Result_Specifications.csv"
+
+# ps_specs_csv = r"J:\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\VSA_Event_Maps_Test_Jupyter\Import_Specs\PS_Capacity_VSA.csv"
+# summation_csv = r"J:\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\VSA_Event_Maps_Test_Jupyter\Import_Specs\Summation.csv"
+# summation_ps_csv = r"J:\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\VSA_Event_Maps_Test_Jupyter\Import_Specs\Summation_PS.csv"
+
+# outfall_csv = r"J:\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\VSA_Event_Maps_Test_Jupyter\Import_Specs\Outfall_Summary.csv"
+
+# map_folder = r"J:\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\VSA_Event_Maps_Test_Jupyter"
+# result_folder = r"J:\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\Design Storms\MODEL"
+
+
+model_area = "NSSA"
+
+result_specs_csv = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard\Import_Specs\Result_Specifications.csv"
+
+ps_specs_csv = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard\Import_Specs\PS_Capacity_NSSA.csv"
+summation_csv = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard\Import_Specs\Summation.csv"
+summation_ps_csv = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard\Import_Specs\Summation_PS.csv"
+
+outfall_csv = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard\Import_Specs\Outfall_Summary.csv"
+
+map_folder = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard"
+result_folder = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model"
