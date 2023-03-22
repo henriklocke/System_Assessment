d362f2c1d49ee1a31f0245914e3c392d5813b6db
henriklocke <henriklocke@hotmail.com>
2023-03-22 14:57:20 -0700

    Add files via upload
    Set flow and volume to 0 if max absolute flow under a user defined threshold, to ignore artificial flow in empty pipes.

--- System_Assessment_Variables.py
+++ System_Assessment_Variables.py
@@ -2,7 +2,71 @@
 
 ##Remember to insert r in front of all paths, e.g. r"J:\SEWER_AREA_MODELS\FSA\03_SIMULATION_WORK\Calibration_2022\MODEL"
 
-# model_area = "VSA"
+
+model_area = "VSA"
+
+result_specs_csv = r"\\prdsynfile01\lws_modelling\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\VSA_Event_Maps_DWF\Import_Specs\Result_Specifications.csv"
+
+ps_specs_csv = r"\\prdsynfile01\lws_modelling\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\VSA_Event_Maps_DWF\Import_Specs\PS_Capacity_VSA.csv"
+summation_csv = r"\\prdsynfile01\lws_modelling\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\VSA_Event_Maps_DWF\Import_Specs\Summation.csv"
+summation_ps_csv = r"\\prdsynfile01\lws_modelling\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\VSA_Event_Maps_DWF\Import_Specs\Summation_PS.csv"
+
+outfall_csv = r"\\prdsynfile01\lws_modelling\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\VSA_Event_Maps_DWF\Import_Specs\Outfall_Summary.csv"
+
+map_folder = r"J:\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\DWF_No_Tide\RESULTS\Debug"
+result_folder = r"J:\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\DWF_No_Tide\RESULTS\Debug"
+
+set_pipe_volume_0_when_max_pipe_flow_less_than_cms = 0.0003
+
+
+# #NSSA Training Session
+# model_area = "NSSA"
+
+# result_specs_csv = r"J:\TOOLS\Training\Week_45_System_Assessment\Exercices-Copy-To-Your-Own-Computer\Import_Specs\Result_Specifications.csv"
+# ps_specs_csv = r"J:\TOOLS\Training\Week_45_System_Assessment\Exercices-Copy-To-Your-Own-Computer\Import_Specs\PS_Capacity_NSSA.csv"
+# summation_csv = r"J:\TOOLS\Training\Week_45_System_Assessment\Exercices-Copy-To-Your-Own-Computer\Import_Specs\Summation.csv"
+# summation_ps_csv = r"J:\TOOLS\Training\Week_45_System_Assessment\Exercices-Copy-To-Your-Own-Computer\Import_Specs\Summation_PS.csv"
+# outfall_csv = r"J:\TOOLS\Training\Week_45_System_Assessment\Exercices-Copy-To-Your-Own-Computer\Import_Specs\Outfall_Summary.csv"
+
+# map_folder = r"J:\TOOLS\Training\Week_45_System_Assessment\Test\Maps"
+# result_folder = r"J:\TOOLS\Training\Week_45_System_Assessment\Results"
+
+
+
+
+
+
+# #FSA System Assessment
+# model_area = "FSA"
+
+# result_specs_csv = r"J:\SEWER_AREA_MODELS\FSA\03_SIMULATION_WORK\Hyetograph_Trial\Model\SA_Maps\Import_Specs\Result_Specifications.csv"
+
+# ps_specs_csv = r"J:\SEWER_AREA_MODELS\FSA\03_SIMULATION_WORK\Hyetograph_Trial\Model\SA_Maps\Import_Specs\PS_Capacity_FSA.csv"
+# summation_csv = r"J:\SEWER_AREA_MODELS\FSA\03_SIMULATION_WORK\Hyetograph_Trial\Model\SA_Maps\Import_Specs\Summation.csv"
+# summation_ps_csv = r"J:\SEWER_AREA_MODELS\FSA\03_SIMULATION_WORK\Hyetograph_Trial\Model\SA_Maps\Import_Specs\Summation_PS.csv"
+
+# outfall_csv = r"J:\SEWER_AREA_MODELS\FSA\03_SIMULATION_WORK\Hyetograph_Trial\Model\SA_Maps\Import_Specs\Outfall_Summary.csv"
+
+# map_folder = r"\\prdsynfile01\lws_modelling\SEWER_AREA_MODELS\FSA\03_SIMULATION_WORK\Hyetograph_Trial\Model\SA_Maps"
+# result_folder = r"J:\SEWER_AREA_MODELS\FSA\03_SIMULATION_WORK\Hyetograph_Trial\Model"
+
+
+
+# #FSA System Assessment
+# model_area = "FSA"
+
+# result_specs_csv = r"J:\SEWER_AREA_MODELS\FSA\04_ANALYSIS_WORK\System_Assessment\System_Assessment_Mapping\FSA_Event_Maps\Import_Specs\Result_Specifications.csv"
+
+# ps_specs_csv = r"J:\SEWER_AREA_MODELS\FSA\04_ANALYSIS_WORK\System_Assessment\System_Assessment_Mapping\FSA_Event_Maps\Import_Specs\PS_Capacity_FSA.csv"
+# summation_csv = r"J:\SEWER_AREA_MODELS\FSA\04_ANALYSIS_WORK\System_Assessment\System_Assessment_Mapping\FSA_Event_Maps\Import_Specs\Summation.csv"
+# summation_ps_csv = r"J:\SEWER_AREA_MODELS\FSA\04_ANALYSIS_WORK\System_Assessment\System_Assessment_Mapping\FSA_Event_Maps\Import_Specs\Summation_PS.csv"
+
+# outfall_csv = r"J:\SEWER_AREA_MODELS\FSA\04_ANALYSIS_WORK\System_Assessment\System_Assessment_Mapping\FSA_Event_Maps\Import_Specs\Outfall_Summary.csv"
+
+# map_folder = r"J:\SEWER_AREA_MODELS\FSA\04_ANALYSIS_WORK\System_Assessment\System_Assessment_Mapping\FSA_Event_Maps"
+# result_folder = r"J:\SEWER_AREA_MODELS\FSA\03_SIMULATION_WORK\System_Assessment\FSA_System_Assessment"
+
+
 
 # result_specs_csv = r"J:\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\VSA_Event_Maps_Test_Jupyter\Import_Specs\Result_Specifications.csv"
 
@@ -16,15 +80,15 @@
 # result_folder = r"J:\SEWER_AREA_MODELS\VSA\04_ANALYSIS_WORK\21. SYSTEM_ASSESSMENT\Design Storms\MODEL"
 
 
-model_area = "NSSA"
+# model_area = "NSSA"
 
-result_specs_csv = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard\Import_Specs\Result_Specifications.csv"
+# result_specs_csv = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard\Import_Specs\Result_Specifications.csv"
 
-ps_specs_csv = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard\Import_Specs\PS_Capacity_NSSA.csv"
-summation_csv = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard\Import_Specs\Summation.csv"
-summation_ps_csv = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard\Import_Specs\Summation_PS.csv"
+# ps_specs_csv = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard\Import_Specs\PS_Capacity_NSSA.csv"
+# summation_csv = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard\Import_Specs\Summation.csv"
+# summation_ps_csv = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard\Import_Specs\Summation_PS.csv"
 
-outfall_csv = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard\Import_Specs\Outfall_Summary.csv"
+# outfall_csv = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard\Import_Specs\Outfall_Summary.csv"
 
-map_folder = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard"
-result_folder = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model"
+# map_folder = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model\SA_Maps_External_Standard"
+# result_folder = r"J:\SEWER_AREA_MODELS\NSSA\03_SIMULATION_WORK\X-ADWF\Model"
