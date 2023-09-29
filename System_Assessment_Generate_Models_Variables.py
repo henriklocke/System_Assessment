c9f01a6b6dd372395a1c8e5b88fde9234dffaca4
henriklocke <henriklocke@hotmail.com>
2023-09-29 16:11:27 -0700

    Add files via upload
    Inserting AD but far from finished

--- System_Assessment_Generate_Models_Variables.py
+++ System_Assessment_Generate_Models_Variables.py
@@ -7,18 +7,22 @@
 model_area = 'FSA'
 generate_future = False
 generate_bsf = False
-generate_xadwf = True
+generate_xadwf = False
 generate_sealed_vfd = False
-
+generate_ad = True
 
 output_folder = r'J:\SEWER_AREA_MODELS\FSA\03_SIMULATION_WORK\System_Assessment\Model_Generation'
 script_path = r"J:\TOOLS\Generate_WaterspillDischarge_C#\WaterspillDischarge.cs"
 
+#For all models:
+version = 105
+
 #For future models
-model_original = r"J:\SEWER_AREA_MODELS\FSA\01_MASTER_MODEL\MODEL\FSA_Base_2021pop.sqlite"
+model_original = r"\\prdsynfile01\lws_modelling\SEWER_AREA_MODELS\FSA\01_MASTER_MODEL\MODEL\BACKUP\FSA_Base_2021pop_Backup106.sqlite"
+# model_original = r"J:\SEWER_AREA_MODELS\FSA\01_MASTER_MODEL\MODEL\FSA_Base_2021pop.sqlite"
 year_original = 2021
-version = 104
 year_scenario_list = []
+year_scenario_list.append([2021,'Base',['Ex']])
 year_scenario_list.append([2030,'2030_Network',['Ex']])
 year_scenario_list.append([2040,'2030_Network',['2050M','2050H']])
 year_scenario_list.append([2050,'2030_Network',['2050H']])
@@ -43,4 +47,13 @@ gwis = [11.2]
 
 #For X ADWF
 times_adwf_list = [3]
+
+#For AD
+dfs0_file = "NSSA_Runoff_EX-5yr-24hr-AES_BaseRR.dfs0"
+model_suffix = '_5y24h_Tracer_WW'
+tracer_csv = 'Tracer_Zones.csv'
+boundary_tracer_ext = '_t' #added to boundary item id for tracers
+sim_ext = "AD_"
+
+
 
